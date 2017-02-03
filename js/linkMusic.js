194186e565f6585553403f72e287a9667847be60
Julien Magnin <Juju@imac-de-sebastien-magnin.home>
2017-02-03 19:11:44 +0100

    kek

---
+++ js/linkMusic.js
@@ -0,0 +1,66 @@
+$(function() {
+  $('#linkMusic0').click(function() {
+    $('#linkMusic0').css('color', 'green');
+	$('#linkMusic0').css('text-decoration', 'none');
+	$('#linkMusic1').css('color', 'white');
+	$('#linkMusic1').css('text-decoration', 'underline');
+	$('#linkMusic2').css('color', 'white');
+	$('#linkMusic2').css('text-decoration', 'underline');
+	$('#linkMusic3').css('color', 'white');
+	$('#linkMusic3').css('text-decoration', 'underline');
+	$('#linkMusic4').css('color', 'white');
+	$('#linkMusic4').css('text-decoration', 'underline');
+  });
+  
+  $('#linkMusic1').click(function() {
+    $('#linkMusic0').css('color', 'white');
+	$('#linkMusic0').css('text-decoration', 'underline');
+	$('#linkMusic1').css('color', 'green');
+	$('#linkMusic1').css('text-decoration', 'none');
+	$('#linkMusic2').css('color', 'white');
+	$('#linkMusic2').css('text-decoration', 'underline');
+	$('#linkMusic3').css('color', 'white');
+	$('#linkMusic3').css('text-decoration', 'underline');
+	$('#linkMusic4').css('color', 'white');
+	$('#linkMusic4').css('text-decoration', 'underline');
+  });
+  
+  $('#linkMusic2').click(function() {
+    $('#linkMusic0').css('color', 'white');
+	$('#linkMusic0').css('text-decoration', 'underline');
+	$('#linkMusic1').css('color', 'white');
+	$('#linkMusic1').css('text-decoration', 'underline');
+	$('#linkMusic2').css('color', 'green');
+	$('#linkMusic2').css('text-decoration', 'none');
+	$('#linkMusic3').css('color', 'white');
+	$('#linkMusic3').css('text-decoration', 'underline');
+	$('#linkMusic4').css('color', 'white');
+	$('#linkMusic4').css('text-decoration', 'underline');
+  });
+  
+  $('#linkMusic3').click(function() {
+    $('#linkMusic0').css('color', 'white');
+	$('#linkMusic0').css('text-decoration', 'underline');
+	$('#linkMusic1').css('color', 'white');
+	$('#linkMusic1').css('text-decoration', 'underline');
+	$('#linkMusic2').css('color', 'white');
+	$('#linkMusic2').css('text-decoration', 'underline');
+	$('#linkMusic3').css('color', 'green');
+	$('#linkMusic3').css('text-decoration', 'none');
+	$('#linkMusic4').css('color', 'white');
+	$('#linkMusic4').css('text-decoration', 'underline');
+  });
+  
+  $('#linkMusic4').click(function() {
+    $('#linkMusic0').css('color', 'white');
+	$('#linkMusic0').css('text-decoration', 'underline');
+	$('#linkMusic1').css('color', 'white');
+	$('#linkMusic1').css('text-decoration', 'underline');
+	$('#linkMusic2').css('color', 'white');
+	$('#linkMusic2').css('text-decoration', 'underline');
+	$('#linkMusic3').css('color', 'white');
+	$('#linkMusic3').css('text-decoration', 'underline');
+	$('#linkMusic4').css('color', 'green');
+	$('#linkMusic4').css('text-decoration', 'none');
+  });
+});
